52ccf4654fb1121d41a9b9b24276ce7599f0ad61
JAWAD T P <96516021+Jawad6940@users.noreply.github.com>
2022-07-15 19:49:51 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,21 @@
 // arr = array of numbers
 function major(arr)
 {
+   let cnt=0;
+  let val=0;
+  arr.forEach(i=>{
+       // console.log(val);
+      if(cnt==0){
+          val=i;
+      }
+      if(val===i){
+          cnt++;
+      }
+      else{
+          cnt--;
+      }
+  })
+ return val; 
   //your code here
 }
 module.exports=major;
